37607052ab9684ba84e93ad833680c4c12f7e0f5
YemaBotha <ST10488809@rcconnect.edu.za>
2026-04-02 12:40:52 +0200

    Added content for ASCII art.cs

--- ASCII art.cs
+++ ASCII art.cs
@@ -1,12 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ChatBotP1
+namespace POEPar1
 {
     internal class ASCII_art
     {
+        public ASCII_art()
+        {
+
+
+            // Set the text colour to cyan for the header
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            // Display the ASCII art logo as the title screen
+            Console.WriteLine(@"                                    /¯¯\
+                  \__/
+                   ||
+                   ||
+                  |  |
+                  |  |
+                  |  |
+                  |  |
+                  |  |
+                  |  |
+              .--.----.--.
+            .-----\__/-----.
+    ___---¯¯////¯¯|\/|¯¯\\\\¯¯---___
+ /¯¯ __O_--////   |  |   \\\\--_O__ ¯¯\
+| O?¯      ¯¯¯    |  |    ¯¯¯      ¯?O | 
+|  '    _.-.      |  |      .-._    '  |
+|O|    ?..?      ./  \.      ?..?    |O|
+| |     '?. .-.  | /\ |  .-. .?'     | |
+| ---__  ¯?__?  /|\¯¯/|\  ?__?¯  __--- |
+|O     \         ||\/ |         /     O|
+|       \  /¯?_  ||   |  _?¯\  /       |
+|       / /    - ||   | -    \ \       |
+|O   __/  | __   ||   |   __ |  \__   O|
+| ---     |/  -_/||   |\_-  \|     --- | 
+|O|            \ ||   | /            |O|  
+\ '              ||   |        ^~DLF ' /
+ \O\    _-¯?.    ||   |    .?¯-_    /O/
+  \ \  /  /¯¯¯?  ||   |  ?¯¯¯\  \  / /
+   \O\/   |      ||   |      |   \/O/
+    \     |      ||   |      |     /
+     '.O  |_     ||   |     _|  O.'
+        '._O'.__/||   |\__.'O_.'
+           '._ O ||   | O _.'
+              '._||   |_.'
+                 ||   |
+                 ||   |
+                 | \/ |
+                 |  | |
+                  \ |/
+                   \/            
+
+   _   _   _   _   _   _   _     _   _   _     _   _   _   _  
+  / \ / \ / \ / \ / \ / \ / \   / \ / \ / \   / \ / \ / \ / \ 
+ ( K | E | E | P | I | N | G ) ( Y | O | U ) ( S | A | F | E )
+  \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/   \_/ \_/ \_/ \_/ 
+   _   _     _   _   _   _   _   _   _     _   _   _   _   _  
+  / \ / \   / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ 
+ ( I | N ) ( D | I | G | I | T | A | L ) ( W | O | R | L | D )
+  \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ 
+
+"
+
+
+);
+
+            // Reset colour back to default after the header
+            Console.ResetColor();
+            Console.WriteLine();
+        }
     }
 }
